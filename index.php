e27b2761efbc711f1947b7f2f88dcd5e0e3aecc5
timbuk <ivoschwizer@gmail.com>
2018-08-08 14:19:45 +0200

    first code

---
+++ index.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="de-CH">
+<head>
+    <?php include 'include/head.php'; ?>
+    <meta name="description" content="..."><!-- definieren -->
+</head>
+
+<body>
+    
+    <div class="button"></div>
+    
+    <div class="grid">
+        <div class="panel-1">
+            <h1 class="content inview">
+                Für die komplexe und zeitintensive Entwicklung von Immobilien-Projekten sind Know-How, Flexibilität und Ausdauer wichtige Erfolgsfaktoren. 
+            </h1>
+        </div>
+        
+        <div class="panel-2">
+           <div class="img-1 inview"></div>
+        </div>
+        
+        <div class="panel-3">
+            <div class="img-2 inview"></div>
+        </div>
+    </div>
+    
+    
+    
+    <script src="js/vendor/modernizr-3.6.0.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="js/vendor/jquery-3.3.1.min.js"><\/script>')</script>
+    <script src="js/plugins.js"></script>
+    <script src="js/main.js"></script>
+ 
+</body>
+
+</html>
---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+
+    // Story FadeIn
+    /*
+    $(window).scroll(function() {
+        $('.inview').each(function(i) {
+            var bottom_of_object = $(this).offset().top;
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            if( bottom_of_window > bottom_of_object ) {
+                $(this).addClass('inview-fade');
+            } else {
+                $(this).removeClass('inview-fade');
+            }
+        }); 
+    }).scroll();
+    */
+});
+
+
